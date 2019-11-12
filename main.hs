4d4eb0ac33ca650e54d1e1e6e3b6b36241937904
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-12 21:41:47 +0000

    Add new class, update main example

--- main.hs
+++ main.hs
@@ -1,5 +1,10 @@
 import Graphics.UI.GLUT
- 
+
+--stack ghc -- --make main.hs
+
+myPoints :: [(GLfloat,GLfloat,GLfloat)]
+myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
 main :: IO ()
 main = do
   (_progName, _args) <- getArgsAndInitialize
@@ -8,6 +13,9 @@ main = do
   mainLoop
  
 display :: DisplayCallback
-display = do
-  clear [ ColorBuffer ]
-  flush
+display = do 
+clear [ColorBuffer]
+renderPrimitive Points $
+    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+flush
+                                 
