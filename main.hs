ee8290f42844393f66acc1a97917979ef723fc6f
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-12 22:47:38 +0000

    Background colour

--- main.hs
+++ main.hs
@@ -1,4 +1,6 @@
 import Graphics.UI.GLUT
+import qualified Graphics.Rendering.OpenGL as GL
+-- import LSystem For future use
 
 --stack ghc -- --make main.hs
 --credit LSystem.hs
@@ -15,9 +17,16 @@ theCanvas  =  Pnt 800 800
 pointToSize :: Pnt -> Size
 pointToSize (Pnt x y) = Size (ceiling x) (ceiling y)
 
+background :: IO ()
+background = do GL.color theBGcolor
+                GL.renderPrimitive GL.Polygon $ mapM_ GL.vertex
+                      [GL.Vertex3 (-1) (-1) 0,
+                       GL.Vertex3   1  (-1) 0,
+                       GL.Vertex3   1    1  0,
+                       GL.Vertex3 (-1)   1 (0::GL.GLfloat) ]
 
--- theBGcolor :: GL.Color3 GL.GLfloat
--- theBGcolor = penToRGB white
+theBGcolor :: GL.Color3 GL.GLfloat
+theBGcolor = GL.Color3 1.0 1.0 1.0
 
 -- vertex3f :: (Float, Float, Float) -> (GLfloat,GLfloat,GLfloat)
 -- vertex3f (x,y,z) = vertex $ Vertex3 x y (z :: GLfloat)
@@ -35,7 +44,8 @@ main = do
 display :: DisplayCallback
 display = do 
     clear [ColorBuffer]
-    -- background -- for futuer use
+    loadIdentity
+    background -- for futuer use
     renderPrimitive Points $
         mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
     swapBuffers
