cefd07b48526d618d4366b41f2be89e7caf7f50c
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-12 22:43:09 +0000

    Code Frankenstein

--- main.hs
+++ main.hs
@@ -6,6 +6,16 @@ import Graphics.UI.GLUT
 myPoints :: [(GLfloat,GLfloat,GLfloat)]
 myPoints = [ (0.1*k , 0.1*k , 0) | k <- [0..10] ]
 
+data Pnt = Pnt Float Float
+  deriving (Eq,Ord,Show)
+
+theCanvas :: Pnt
+theCanvas  =  Pnt 800 800
+
+pointToSize :: Pnt -> Size
+pointToSize (Pnt x y) = Size (ceiling x) (ceiling y)
+
+
 -- theBGcolor :: GL.Color3 GL.GLfloat
 -- theBGcolor = penToRGB white
 
@@ -14,15 +24,20 @@ myPoints = [ (0.1*k , 0.1*k , 0) | k <- [0..10] ]
 
 main :: IO ()
 main = do
-  (_progName, _args) <- getArgsAndInitialize
-  _window <- createWindow "Hello World"
-  displayCallback $= display
-  mainLoop
+    initialDisplayMode $= [DoubleBuffered]
+    initialWindowSize  $= pointToSize theCanvas 
+    getArgsAndInitialize
+    w <- createWindow "Turtle Graphics"
+    displayCallback $= display
+    reshapeCallback $= Just (\x -> (viewport $= (Position 0 0, x)))
+    mainLoop
  
 display :: DisplayCallback
 display = do 
-clear [ColorBuffer]
-renderPrimitive Points $
-    mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
-flush
+    clear [ColorBuffer]
+    -- background -- for futuer use
+    renderPrimitive Points $
+        mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+    swapBuffers
+
                                 
