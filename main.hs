62666a6554cc05612de570381c19df5732c1dabc
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-13 02:07:36 +0000

    Bezier complete

--- main.hs
+++ main.hs
@@ -31,8 +31,8 @@ pointBezier p0 p1 p2 p3 t = Pnt (x0*sf0 + x1*sf1 + x2*sf2 + x3*sf3) (y0*sf0 + y1
         y2 = y p2
         y3 = y p3
 
-test :: Float -> Pnt
-test = pointBezier (Pnt 0.0 0.0) (Pnt 0.0 1.0) (Pnt 1.0 1.0) (Pnt 1.0 0.0)
+-- test :: Float -> Pnt
+-- test = pointBezier (Pnt 0.0 0.0) (Pnt 0.0 1.0) (Pnt 1.0 0.0) (Pnt 1.0 1.0)
 
 
 myPoints :: [(GLfloat,GLfloat,GLfloat)]
@@ -40,6 +40,35 @@ myPoints = [ (0.1*k , 0.1*k , 0) | k <- [0..10] ]
 
 myBezier :: [(GLfloat,GLfloat,GLfloat)]
 myBezier = [pntToPoint (test t)| t <- map (*0.0001) [0..10000] ]
+        where test = pointBezier (Pnt (-1.0) 0.0) (Pnt 0.0 0.3) (Pnt 0.0 (-0.3)) (Pnt 1.0 0.0)
+
+myBezier1 :: [(GLfloat,GLfloat,GLfloat)]
+myBezier1 = [pntToPoint (test t)| t <- map (*0.0001) [0..10000] ]
+        where test = pointBezier (Pnt (-1.0) 0.0) (Pnt 0.0 0.2) (Pnt 0.0 (-0.2)) (Pnt 1.0 0.0)
+
+myBezier2 :: [(GLfloat,GLfloat,GLfloat)]
+myBezier2 = [pntToPoint (test t)| t <- map (*0.0001) [0..10000] ]
+        where test = pointBezier (Pnt (-1.0) 0.0) (Pnt 0.0 0.1) (Pnt 0.0 (-0.1)) (Pnt 1.0 0.0)
+
+bezier :: (Pnt,Pnt,Pnt,Pnt) -> [(GLfloat,GLfloat,GLfloat)]
+bezier (a,b,c,d) = [pntToPoint (pointBezier a b c d t)| t <- map (*0.0001) [0..10000] ]
+
+pointsList :: [((Float,Float),(Float,Float),(Float,Float),(Float,Float))]
+pointsList = [  ( ( -1.0, 0.0 ) ,  ( 0.0, 0.1 ) ,  ( 0.0, -0.1 ) , ( 1.0, 0.0 ) ),
+                ( ( -1.0, 0.0 ) ,  ( 0.0, 0.2 ) ,  ( 0.0, -0.2 ) , ( 1.0, 0.0 ) ),
+                ( ( -1.0, 0.0 ) ,  ( 0.0, 0.3 ) ,  ( 0.0, -0.3 ) , ( 1.0, 0.0 ) ),
+                ( ( -1.0, 0.0 ) ,  ( 0.0, 0.4 ) ,  ( 0.0, -0.4 ) , ( 1.0, 0.0 ) ),
+                ( ( -1.0, 0.0 ) ,  ( 0.0, 0.5 ) ,  ( 0.0, -0.5 ) , ( 1.0, 0.0 ) ) ]
+
+pointsListToPntList :: [((Float,Float),(Float,Float),(Float,Float),(Float,Float))] -> [(Pnt,Pnt,Pnt,Pnt)]
+pointsListToPntList xs = [(toPnt a, toPnt b, toPnt c, toPnt d) |(a,b,c,d) <- xs]
+        where toPnt (y,z) = Pnt y z
+
+genDrawList :: [(Pnt,Pnt,Pnt,Pnt)] -> [(GLfloat,GLfloat,GLfloat)]
+genDrawList = concatMap bezier
+
+-- BezierLineGruop1 = genDrawList (pointsListToPntList pointsList)
+
 
 pntToPoint :: Pnt -> (GLfloat,GLfloat,GLfloat)
 pntToPoint (Pnt a b) = (a,b,0)
@@ -80,7 +109,7 @@ display = do
     loadIdentity
     -- background -- for futuer use
     renderPrimitive Points $
-        mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myBezier
+        mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) (genDrawList (pointsListToPntList pointsList))
     swapBuffers
 
                                 
