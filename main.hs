f034340ecbbfb7f336c98a226984699f650f95a1
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-14 11:49:47 +0000

    Beta code

--- main.hs
+++ main.hs
@@ -7,16 +7,18 @@ import LinesSet
 --stack ghc -- --make main.hs
 --credit LSystem.hs
 
-data Colour = Colour Float Float Float
-    deriving (Eq,Ord,Show)
+--Change colour profile , background colour and point size here--
+theBGcolor :: GL.Color3 GL.GLfloat
+pSize :: GLfloat
 
-data ColourPointsGLfloat = ColourPointsGLfloat Colour Colour Colour Colour
-    deriving (Eq,Ord,Show)
+colourProfile = redToYellow
+pSize = 1.7
+theBGcolor = GL.Color3 1.0 1.0 1.0
+--End of manual changing--
+
 
 data Pnt = Pnt Float Float
   deriving (Eq,Ord,Show)
-
-colourProfile = redToYellow
 
 x :: Pnt -> Float
 y :: Pnt -> Float
@@ -39,10 +41,10 @@ pointBezier p0 p1 p2 p3 t = Pnt (x0*sf0 + x1*sf1 + x2*sf2 + x3*sf3) (y0*sf0 + y1
         y2 = y p2
         y3 = y p3
 
---No colour bezier function--
-bezier :: (Pnt,Pnt,Pnt,Pnt) -> [(GLfloat,GLfloat,GLfloat)]
-bezier (a,b,c,d) = [pntToPoint (pointBezier a b c d t)| t <- map (*0.0001) [0..10000] ]
---end of No colour function--
+-- --No colour bezier function--
+-- bezier :: (Pnt,Pnt,Pnt,Pnt) -> [(GLfloat,GLfloat,GLfloat)]
+-- bezier (a,b,c,d) = [pntToPoint (pointBezier a b c d t)| t <- map (*0.0001) [0..10000] ]
+-- --end of No colour function--
 
 --new colour bezier function--
 bezierColour :: (Pnt,Pnt,Pnt,Pnt) -> [((GLfloat,GLfloat,GLfloat),(GLfloat,GLfloat,GLfloat))]
@@ -59,7 +61,7 @@ genDrawList = concatMap bezier
 genDrawListColour :: [(Pnt,Pnt,Pnt,Pnt)] -> [((GLfloat,GLfloat,GLfloat),(GLfloat,GLfloat,GLfloat))]
 genDrawListColour = concatMap bezierColour
 
---Helper block--
+--Helper block From LSystem--
 
 pntToPoint :: Pnt -> (GLfloat,GLfloat,GLfloat)
 pntToPoint (Pnt a b) = (a,b,0)
@@ -78,9 +80,6 @@ background = do GL.color theBGcolor
                        GL.Vertex3   1    1  0,
                        GL.Vertex3 (-1)   1 (0::GL.GLfloat) ]
 
-theBGcolor :: GL.Color3 GL.GLfloat
-theBGcolor = GL.Color3 1.0 1.0 1.0
-
 --End of helper block--
 
 main :: IO ()
@@ -88,23 +87,19 @@ main = do
     initialDisplayMode $= [DoubleBuffered]
     initialWindowSize  $= pointToSize theCanvas 
     getArgsAndInitialize
-    w <- createWindow "Turtle Graphics"
+    w <- createWindow "Bezier Curves Colour Wave"
     displayCallback $= display
     reshapeCallback $= Just (\x -> (viewport $= (Position 0 0, x)))
     mainLoop
 
-pSize :: GLfloat
-pSize = 1.7
-
---testing block--
 display :: DisplayCallback
 display = do 
         clear [ColorBuffer]
         loadIdentity
-        background -- for futuer use
+        background 
 
         GL.pointSmooth $= GL.Enabled --smooth
-        GL.pointSize $= pSize -- point size
+        GL.pointSize $= pSize --Point size
 
         forM_ (genDrawListColour (pointsListToPntList pointsList)) $ \((r,g,b),(x,y,z)) ->
                 preservingMatrix $ do
