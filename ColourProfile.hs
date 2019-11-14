240ef654845d2e3e6c6f286bea4d99cde825f844
Hantao Zhong <hantao.zhong@gmail.com>
2019-11-14 09:49:58 +0000

    More tidy. Really going to Lecture NOW Btw ColourProfile has naming bug lol

--- ColourProfile.hs
+++ ColourProfile.hs
@@ -1,7 +1,12 @@
 module ColourProfile
     (
         Colour,
-        ColourProfileRGB
+        redToYellow,
+        yellowToGreen,
+        greenToBlue,
+        blueToPurple,
+        purpleToPink,
+        rainbow
     )
     where
 
@@ -10,20 +15,14 @@ data Colour = Colour Float Float Float
 
 data ColourPointsRGB = ColourPointsRGB Colour Colour Colour Colour
     deriving (Eq,Ord,Show)
---Colour control points
--- Warning 
-tc1_RGB :: Colour
-tc2_RGB :: Colour
-tc3_RGB :: Colour
-tc4_RGB :: Colour
 
 redToYellow :: ColourPointsRGB
 redToYellow = ColourPointsRGB tc1 tc2 tc3 tc4
         where
-        tc1_RGB = Colour 255 255 255
-        tc2_RGB = Colour 204 0 0
-        tc3_RGB = Colour 255 255 0
-        tc4_RGB = Colour 255 255 255
+        tc1 = Colour 255 255 255
+        tc2 = Colour 204 0 0
+        tc3 = Colour 255 255 0
+        tc4 = Colour 255 255 255
 
 yellowToGreen :: ColourPointsRGB
 yellowToGreen = ColourPointsRGB tc1 tc2 tc3 tc4
@@ -41,20 +40,26 @@ greenToBlue = ColourPointsRGB tc1 tc2 tc3 tc4
         tc3_RGB = Colour 51 153 255
         tc4_RGB = Colour 255 255 255
 
---Blue to Purple
--- tc1_RGB = Colour 255 255 255
--- tc2_RGB = Colour 51 153 255
--- tc3_RGB = Colour 153 51 255
--- tc4_RGB = Colour 255 255 255
+blueToPurple :: ColourPointsRGB
+blueToPurple = ColourPointsRGB tc1 tc2 tc3 tc4
+        where
+        tc1_RGB = Colour 255 255 255
+        tc2_RGB = Colour 51 153 255
+        tc3_RGB = Colour 153 51 255
+        tc4_RGB = Colour 255 255 255
 
---Purple to pink
-tc1_RGB = Colour 255 255 255
-tc2_RGB = Colour 153 51 255
-tc3_RGB = Colour 255 0 127
-tc4_RGB = Colour 255 255 255
+purpleToPink :: ColourPointsRGB
+purpleToPink = ColourPointsRGB tc1 tc2 tc3 tc4
+        where
+        tc1_RGB = Colour 255 255 255
+        tc2_RGB = Colour 153 51 255
+        tc3_RGB = Colour 255 0 127
+        tc4_RGB = Colour 255 255 255
 
---RainBow
--- tc1_RGB = Colour 204 0 0
--- tc2_RGB = Colour 255 255 0
--- tc3_RGB = Colour 51 255 51
--- tc4_RGB = Colour 51 51 255
+rainbow :: ColourPointsRGB
+rainbow = ColourPointsRGB tc1 tc2 tc3 tc4
+        where
+        tc1_RGB = Colour 204 0 0
+        tc2_RGB = Colour 255 255 0
+        tc3_RGB = Colour 51 255 51
+        tc4_RGB = Colour 51 51 255
